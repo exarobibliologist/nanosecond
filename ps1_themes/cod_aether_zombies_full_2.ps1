b8f9fd441a025a690a868ea138bb4f281fe8ebcc
Sean Murphy <exarobibliologist@gmail.com>
2026-04-05 22:27:55 -0400

    New Themes
    Some of these were built with the awesome ps1_create script!

---
+++ ps1_themes/cod_aether_zombies_full_2.ps1
@@ -0,0 +1,1 @@
+PS1="\[$(bold 196)\]\d \t => \[$(color 129)\]\u@\h \[$(color 238)\]in \[$(color 196)\]\w \[$(color 129)\]\$=> \[$(reset)\]"
---
+++ ps1_themes/deus_ex_unatco_multiline.ps1
@@ -0,0 +1,1 @@
+export PS1="\[$(bold 51)\]╓╌UNATCO NET\n$(bold 51)\]╟╌\[$(bold 27)\]AUTH \$: \u\n\[$(bold 255)\]╟╌ACTIVE DIR: \w\n\[$(bold 51)\]╙╌COMMAND:\[$(reset)\] "
