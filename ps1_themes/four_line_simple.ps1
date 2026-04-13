257bebf7991c4c892b0e6cb545d09eb63eb04a35
exarobibliologist <exarobibliologist@gmail.com>
2026-04-13 12:22:37 -0400

    New themes and tweaked ps1_select to display 10 previews at once becuase of the sheer number of themes

---
+++ ps1_themes/four_line_simple.ps1
@@ -0,0 +1,1 @@
+export PS1="\[$(color 251)\][[[Authorized User: \u]]] \[$(color 208)\]\n[[[Directory: \w]]] \[$(color 148)\]\n[[[Command History Number: \!]]] \[$(color 160)\]\n[[[Enter Command:>>>\[$(reset)\] "
