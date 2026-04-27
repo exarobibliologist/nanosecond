c3b509f635e437baba2162cb672ab5ffa0d3f4c3
exarobibliologist <exarobibliologist@gmail.com>
2026-04-27 14:40:01 -0400

    Added support for 16 million background colors as well. color and bold can now take 6 inputs!

--- colorize.bash
+++ colorize.bash
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-###################################
-# LOOK! IT'S A RAINBOW OF COLORS! #
-###################################
 color() {
-    # 1. Check for RGB (3 arguments)
+    # 1. Check for RGB Foreground + Background (6 arguments)
+    if [[ $# -eq 6 ]]; then
+        # \e[38;2;... is foreground, \e[48;2;... is background
+        printf "\e[38;2;%s;%s;%sm\e[48;2;%s;%s;%sm" "$1" "$2" "$3" "$4" "$5" "$6"
+        return 0
+    fi
+
+    # 2. Check for RGB Foreground only (3 arguments)
     if [[ $# -eq 3 ]]; then
         printf "\e[38;2;%s;%s;%sm" "$1" "$2" "$3"
         return 0
     fi
 
-    # 2. Check for Hex or ANSI (1 argument)
+    # 3. Check for Hex/ANSI Foreground + Background (2 arguments)
+    if [[ $# -eq 2 ]]; then
+        local fg="${1//#/}"
+        local bg="${2//#/}"
+
+        # If both are Hex
+        if [[ "$fg" =~ ^[0-9a-fA-F]{6}$ ]] && [[ "$bg" =~ ^[0-9a-fA-F]{6}$ ]]; then
+            printf "\e[38;2;%d;%d;%dm\e[48;2;%d;%d;%dm" "0x${fg:0:2}" "0x${fg:2:2}" "0x${fg:4:2}" "0x${bg:0:2}" "0x${bg:2:2}" "0x${bg:4:2}"
+            return 0
+        fi
+
+        # If both are ANSI
+        if [[ "$fg" =~ ^[0-9]+$ ]] && [ "$fg" -ge 0 ] && [ "$fg" -le 255 ] && \
+           [[ "$bg" =~ ^[0-9]+$ ]] && [ "$bg" -ge 0 ] && [ "$bg" -le 255 ]; then
+            printf "\e[38;5;%sm\e[48;5;%sm" "$fg" "$bg"
+            return 0
+        fi
+    fi
+
+    # 4. Check for Hex or ANSI Foreground only (1 argument)
     if [[ $# -eq 1 ]]; then
         local val="${1//#/}" # Strip out '#' if present
 
-        # Check if it's Hex (exactly 6 hex characters)
+        # Check if it's Hex
         if [[ "$val" =~ ^[0-9a-fA-F]{6}$ ]]; then
             printf "\e[38;2;%d;%d;%dm" "0x${val:0:2}" "0x${val:2:2}" "0x${val:4:2}"
             return 0
         fi
 
-        # Check if it's ANSI (number between 0 and 255)
+        # Check if it's ANSI
         if [[ "$val" =~ ^[0-9]+$ ]] && [ "$val" -ge 0 ] && [ "$val" -le 255 ]; then
             printf "\e[38;5;%sm" "$val"
             return 0
@@ -32,13 +55,36 @@ color() {
 }
 
 bold() {
-    # 1. Check for RGB (3 arguments)
+    # 1. Check for RGB Foreground + Background (6 arguments)
+    if [[ $# -eq 6 ]]; then
+        printf "\e[1;38;2;%s;%s;%sm\e[48;2;%s;%s;%sm" "$1" "$2" "$3" "$4" "$5" "$6"
+        return 0
+    fi
+
+    # 2. Check for RGB Foreground only (3 arguments)
     if [[ $# -eq 3 ]]; then
         printf "\e[1;38;2;%s;%s;%sm" "$1" "$2" "$3"
         return 0
     fi
 
-    # 2. Check for Hex or ANSI (1 argument)
+    # 3. Check for Hex/ANSI Foreground + Background (2 arguments)
+    if [[ $# -eq 2 ]]; then
+        local fg="${1//#/}"
+        local bg="${2//#/}"
+
+        if [[ "$fg" =~ ^[0-9a-fA-F]{6}$ ]] && [[ "$bg" =~ ^[0-9a-fA-F]{6}$ ]]; then
+            printf "\e[1;38;2;%d;%d;%dm\e[48;2;%d;%d;%dm" "0x${fg:0:2}" "0x${fg:2:2}" "0x${fg:4:2}" "0x${bg:0:2}" "0x${bg:2:2}" "0x${bg:4:2}"
+            return 0
+        fi
+
+        if [[ "$fg" =~ ^[0-9]+$ ]] && [ "$fg" -ge 0 ] && [ "$fg" -le 255 ] && \
+           [[ "$bg" =~ ^[0-9]+$ ]] && [ "$bg" -ge 0 ] && [ "$bg" -le 255 ]; then
+            printf "\e[1;38;5;%sm\e[48;5;%sm" "$fg" "$bg"
+            return 0
+        fi
+    fi
+
+    # 4. Check for Hex or ANSI Foreground only (1 argument)
     if [[ $# -eq 1 ]]; then
         local val="${1//#/}"
 
@@ -56,26 +102,6 @@ bold() {
     return 1
 }
 
-reset() { printf "\e[0m"; }              # Reset to default
-
-function color_rainbow() {
-    for i in {0..255}; do
-        printf "\e[38;5;%sm color %s\e[0m\n" "$i" "$i"
-    done | less -R
-}
-
-function bold_rainbow() {
-    for i in {0..255}; do
-        printf "\e[1;38;5;%sm bold %s\e[0m\n" "$i" "$i"
-    done | less -R
-}
-
-function color_waterfall() {
-    while : ; do
-        for i in {0..255}; do
-            # %s drops the variable $i into the ANSI code and the text string
-            printf "\e[38;5;%sm Color%s\e[0m\n" "$i" "$i"
-            sleep .1
-        done
-    done
+reset() {
+    printf "\e[0m"
 }
