a6862f2aca33b73ef5e3c89992abdad29859469f
exarobibliologist <exarobibliologist@gmail.com>
2026-04-28 08:02:22 -0400

    Squashed bug. Cleaned bug guts. Theme polished like new.

--- ps1_themes/deus_ex_unatco_multiline.ps1
+++ ps1_themes/deus_ex_unatco_multiline.ps1
@@ -1,1 +1,1 @@
-\[$(bold 51)\]╓╌UNATCO NET\n$(bold 51)\]╟╌\[$(bold 27)\]AUTH \$: \u\n\[$(bold 255)\]╟╌ACTIVE DIR: \w\n\[$(bold 51)\]╙╌COMMAND:\[$(reset)\] "
+export PS1="\[$(bold 51)\]╓╌UNATCO NET\n$(bold 51)\]╟╌\[$(bold 27)\]AUTH \$: \u\n$(bold 51)╟╌\[$(bold 255)\]ACTIVE DIR: \w\n\[$(bold 51)\]╙╌COMMAND:\[$(reset)\] "
